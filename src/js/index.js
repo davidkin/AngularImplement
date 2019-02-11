0efb50b266769f50abafdddabfb3a4b8b438e2d5
David <davidfloyd322@gmail.com>
2019-02-11 17:52:56 +0200

    master, init

---
+++ src/js/index.js
@@ -0,0 +1,19 @@
+(function() {
+  const directives = [{}];
+
+  const smallAngular = {
+    directive() {
+      return null;
+    },
+
+    compile(node) {
+      return null;
+    },
+
+    bootstrap(node) {
+      return null;
+    }
+  };
+
+  window.smallAngular = smallAngular;
+}());
